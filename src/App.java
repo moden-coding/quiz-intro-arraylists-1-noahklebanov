adbef5aa085cd2f109c1ee4e19e7cda7cdbb75ae
noahklebanov <27nklebanov@gannacademy.org>
2024-10-31 12:54:22 -0400

    done

--- src/App.java
+++ src/App.java
@@ -2,14 +2,20 @@ import java.util.*;
 
 public class App {
 
-    //Write code that will:
-    //1. Store the following values in an ArrayList: "Java", "Python", "JavaScript"
-    //   The name of the ArrayList is up to you.
-    //2. Print the contents of your ArrayList using loops. This should work even if 
-    //   I add more elements to your ArrayList. For example, I might add "Ruby" to the
-    //   ArrayList and this should still work.
+    // Write code that will:
+    // 1. Store the following values in an ArrayList: "Java", "Python", "JavaScript"
+    // The name of the ArrayList is up to you.
+    // 2. Print the contents of your ArrayList using loops. This should work even if
+    // I add more elements to your ArrayList. For example, I might add "Ruby" to the
+    // ArrayList and this should still work.
     public static void main(String[] args) throws Exception {
-    
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Java");
+        list.add("Python");
+        list.add("JavaScript");
+        for (String print : list) {
+            System.out.println(print);
+        }
 
     }
 }
